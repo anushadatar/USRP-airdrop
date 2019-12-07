5c9dea190deb9298846f63a0b168cb7c20e4d9c4
vivienyuwenchen <vivienyuwenchen@gmail.com>
2019-12-07 18:36:39 -0500

    Add 3x redundancy

--- pack_data.m
+++ pack_data.m
@@ -1,3 +1,4 @@
 function prepared_data = pack_data(compressed_data)
-    prepared_data = compressed_data;
+    % repeat each bit 3 times for redundancy
+    prepared_data = repelem(compressed_data,3);
 end 
