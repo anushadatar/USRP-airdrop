e11e564d526d851590ac15bf011f299f0d9e9c67
Anusha Datar <anushadatar@gmail.com>
2019-12-07 16:00:08 -0500

    sometimes the things that were useful to you in the past are not useful to you in the present

--- receive_image.m
+++ receive_image.m
@@ -1,5 +1,8 @@
 function[] = receive_image(rx_filename)
+    % Unpack data using filename, correct for error across the channel.
     unpacked_data = unpack_data(rx_filename);
+    % Run the decompression algorithm to recover the image.
     decompressed_data = decompress(unpacked_data);
+    % Convert the decompressed data to an actual image. 
     data2photo(decompressed_data);
 end
