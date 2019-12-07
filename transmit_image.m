cf5e9d1cabdb2b680066af759d439c127c0cdd22
vivienyuwenchen <vivienyuwenchen@gmail.com>
2019-12-07 16:10:37 -0500

    Edit output of photo2data to just packet

--- transmit_image.m
+++ transmit_image.m
@@ -1,10 +1,10 @@
 function[] = transmit_image(image_file, tx_filename)
-% TODO: Remove image size
-[im_size, packet] = photo2data(image_file);
-% compress
-compressed_data = compress(packet);
-% add redundancy and known bits
-prepared_data = prepare_data_for_transmission(compressed_data);
-% save file somewhere send over channel
-write_usrp_data_file_function(prepared_data, tx_filename);
+    % TODO: Remove image size
+    packet = photo2data(image_file);
+    % compress
+    compressed_data = compress(packet);
+    % add redundancy and known bits
+    prepared_data = prepare_data_for_transmission(compressed_data);
+    % save file somewhere send over channel
+    write_usrp_data_file_function(prepared_data, tx_filename);
 end
