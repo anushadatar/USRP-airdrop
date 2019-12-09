cad570a331d56bb0a268297d9a300c9dea9d1e37
Anusha Datar <anushadatar@gmail.com>
2019-12-09 18:45:40 -0500

    incremental docs improvement

--- pack_data.m
+++ pack_data.m
@@ -1,4 +1,5 @@
 function prepared_data = pack_data(compressed_data)
     % repeat each bit 3 times for redundancy
     prepared_data = repelem(compressed_data,3);
+
 end 
--- transmit_image.m
+++ transmit_image.m
@@ -1,29 +1,16 @@
 function[] = transmit_image(image_file, tx_filename)
-    % obtain binary representation of photo
+    % Prepare a file to send the image using the transmitter.
+    % Input: image_file  - The path to the image file to save the data of.
+    %        tx_filename - The path to the file to save the transmit data to.
+    %        
+    % Output: Saves the image data to the tx file. 
+
+    % Create a binary representation of the photo.
     packet = photo2data(image_file);
-    % compress
+    % Compress the data.
     compressed_data = compress(packet);
-    % add redundancy and known bits
+    % Add redundancy and known bits.
     packed_data = pack_data(compressed_data);
-    
-    % prepared packed_data for transmission
-    % replace 0s with -1s
-    packed_data(packed_data == 0) = -1;
-    % add extra bit if length is odd
-    if mod(packed_data, 2) == 1
-        packed_data = [packed_data -1];
-    end
-    % I contains odd bits, Q contains even bits
-    bits_I = packed_data(1:2:end);
-    bits_Q = packed_data(2:2:end);
-    m_k = bits_I+j*bits_Q;
-    % convolve with pulse
-    pulse_length = 100;
-    p = ones(pulse_length,1);
-    x = conv(upsample(m_k, pulse_length), p);
-    
-    % write to file
-    f1 = fopen(tx_filename, 'w');
-    fwrite(f1, x, 'float32');
-    fclose(f1);
+    % Save the data to the specified file location.
+    save_data_to_file(packed_data, tx_filename)
 end
