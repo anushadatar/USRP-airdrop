75edddc82701e89be39c606d898eec6875ba2453
vivienyuwenchen <vivienyuwenchen@gmail.com>
2019-12-09 20:34:25 -0500

    Add length of compressed data

--- pack_data.m
+++ pack_data.m
@@ -2,6 +2,11 @@ function prepared_data = pack_data(compressed_data)
     % add known bits and pad end with 0s
     final_length = 100000;
     known_data = ones(1,200);
-    known_compressed = [known_data compressed_data];
+    compressed_length = de2bi(length(compressed_data), 20, 'left-msb');
+    known_compressed = [known_data compressed_length compressed_data];
     prepared_data = [known_compressed zeros(1,final_length-length(known_compressed))];
+    % 1:200 - known data (1s)
+    % 201:220 - length of compressed data (in binary)
+    % 221:(220+length(compressed_data)) - compressed data
+    % rest is padding (0s)
 end
