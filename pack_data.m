2f157b836da6368b000d689cca238472e262b7c4
vivienyuwenchen <vivienyuwenchen@gmail.com>
2019-12-09 19:42:46 -0500

    Add known bits and pad end with 0s

--- pack_data.m
+++ pack_data.m
@@ -1,4 +1,7 @@
 function prepared_data = pack_data(compressed_data)
-    % repeat each bit 3 times for redundancy
-    
-end 
+    % add known bits and pad end with 0s
+    final_length = 100000;
+    known_data = ones(1,200);
+    known_compressed = [known_data compressed_data];
+    prepared_data = [known_compressed zeros(1,final_length-length(known_compressed))];
+end
