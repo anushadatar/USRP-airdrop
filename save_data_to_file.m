3caf9b78c15c0769c61c4e1705f2f0b9f39a25f3
Anusha Datar <anushadatar@gmail.com>
2019-12-10 21:15:50 -0500

    made save data to file save the data to the file.

--- save_data_to_file.m
+++ save_data_to_file.m
@@ -1,21 +1,10 @@
 function[] = save_data_to_file(packed_data, tx_filename)    
-    % Save packed data to 
-    % Amplitude value for the signal.
-    amplitude = 0.5;
-    % Replace 0s with -1s.
-    packed_data(packed_data == 0) = -1;
-    
-    % I contains odd bits, Q contains even bits
-    bits_I = packed_data(1:2:end);
-    bits_Q = packed_data(2:2:end);
-    m_k = amplitude*bits_I+j*bits_Q;
-    % Convolve the data bits with a pulse.
-    pulse_length = 20;
-    p = ones(pulse_length,1);
-    x = conv(upsample(m_k, pulse_length), p);
-    
-    % write to file
+    % Save packed data to a file ready for transmit.
+    % Input: packed_data = Bits prepared for transmission, with known.
+    %        tx_filename = Path to file to store transmit information in.
+    % Output: Saves packed_data to file at tx_filename.
+    % Write to file with specified filename.
     f1 = fopen(tx_filename, 'w');
-    fwrite(f1, x, 'float32');
+    fwrite(f1, packed_data, 'float32');
     fclose(f1);
 end
