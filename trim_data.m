5b4f534c3c05e8a9438ec6820e33fe3560796d66
lanfenson <laurenanfenson@gmail.com>
2019-12-12 01:06:54 -0500

    i'm a dumby and these are dependencies

--- trim_data.m
+++ trim_data.m
@@ -1,4 +1,4 @@
-function  [trimmed_w_known, trimmed_no_known] = trim_data(rx)
+function  [trimmed_w_known, trimmed_no_known, known_sig] = trim_data(rx, pulse_size)
 %function inputs recieved raw data and ouputs cleaned data with front end noise removed and "known bits" removed. 
 %It also outputs the angle in which
 %the data is rotated in the constellation for future correction. 
@@ -13,22 +13,23 @@ function  [trimmed_w_known, trimmed_no_known] = trim_data(rx)
 %goals:
 %remove beginning noise find time in which data transmission starts
 %find angle in which it is rotatated. 
-    f1 = fopen(rx, 'r');
-    tmp = fread(f1,'float32');
-    fclose(f1);
-    rx = tmp(1:2:end)+1i*tmp(2:2:end);
-    len_transmit = 2500050
+    %f1 = fopen(rx, 'r');
+    %tmp = fread(f1,'float32');
+    %fclose(f1);
+    %rx = tmp(1:2:end)+1i*tmp(2:2:end);
+    len_transmit = 25000000
 
 %full_sig = datastream
 %figure(1)
 %plot(real(rx))
-p = ones(50,1);
+p = ones(pulse_size,1);
 x = conv(rx, p);
-plot(real(x))
+%plot(real(x))
 t = linspace(0,20000,20000);
-load('known_sig.mat');
+%load('known_sig_5000.mat');
+%known_sig = x(8386000:8386000+80000);
 %known_sig = cos(10*pi*(.015)*t);
-% known_sig = ones(4000,1)';
+known_sig = 0.003.*ones(200.*pulse_size,1)';
 [xCorr,lags] = xcorr(x,known_sig);
 % %best known signal for data 
 % %figure(2)
@@ -37,15 +38,15 @@ load('known_sig.mat');
 % %title('xcor')
 % %
 [~,I] = max(abs(xCorr));
-maxt = lags(I)
+maxt = lags(I) + 1500
 new_rx = rx(maxt:len_transmit+maxt-1);
-newer_rx_no_known = rx(maxt+10000:len_transmit+maxt-1);
+newer_rx_no_known = rx(maxt+(2000.*pulse_size):len_transmit+maxt-1);
 % figure(1)
 % hold on
-figure(2)
-plot(real(new_rx))
-figure(3)
-plot(real(newer_rx_no_known))
+%figure(2)
+%plot(real(new_rx))
+%figure(3)
+%plot(real(newer_rx_no_known))
 % %
 %  filtered_data = newer_rx
 trimmed_w_known = new_rx;
